d2cbcd1b087ea1c896b382280df9c0de69c06182
Rohan Prabhu <rohan@mitter.io>
2018-08-11 18:13:41 +0530

    Use isomorphic fetch

--- packages/mitter-ts-core/src/services/channels.ts
+++ packages/mitter-ts-core/src/services/channels.ts
@@ -1,12 +1,11 @@
+import fetch from 'isomorphic-fetch'
 import { MitterConstants } from './constants'
 
 export class MitterChannels {
-    constructor(
-        private readonly mitterApiUrl: string = MitterConstants.MitterApiUrl
-    ) {}
+    constructor(private readonly mitterApiUrl: string = MitterConstants.MitterApiUrl) {}
 
     async getParticipatedChannels(userId: string | undefined = undefined): Promise<Response> {
-        let userIdParam = (userId === undefined) ? 'me' : userId
+        let userIdParam = userId === undefined ? 'me' : userId
 
         return await fetch(`${this.mitterApiUrl}/v1/users/${userIdParam}/channels`)
     }
