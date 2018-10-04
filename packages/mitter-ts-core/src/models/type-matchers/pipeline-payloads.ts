d37aba8e2c8fad2a01b1b690f2145c21b6d87e1e
Rohan Prabhu <rohan@mitter.io>
2018-10-05 00:18:45 +0530

    Bump to 0.5.7

--- packages/mitter-ts-core/src/models/type-matchers/pipeline-payloads.ts
+++ packages/mitter-ts-core/src/models/type-matchers/pipeline-payloads.ts
@@ -1,22 +1,34 @@
+import {
+    NewMessagePayload,
+    NewChannelTimelineEventPayload,
+    ParticipationChangedEventPayload,
+    NewChannelPayload,
+    NewMessageTimelineEventPayload
+} from '@mitter-io/models'
+
 function generatePipelinePayloadMatcher<T>(payloadType: string): (input: any) => input is T {
     return (input: any): input is T => {
         return input['@type'] !== undefined && input['@type'] === payloadType
     }
 }
 
-export const isNewMessagePayload = generatePipelinePayloadMatcher('new-message-payload')
-export const isNewChannelPayload = generatePipelinePayloadMatcher('new-channel-payload')
-
-export const isNewMessageTimelineEventPayload = generatePipelinePayloadMatcher(
-    'new-message-timeline-event-payload'
+export const isNewMessagePayload = generatePipelinePayloadMatcher<NewMessagePayload>(
+    'new-message-payload'
 )
-export const isNewChannelTimelineEventPayload = generatePipelinePayloadMatcher(
-    'new-channel-timeline-event-payload'
+export const isNewChannelPayload = generatePipelinePayloadMatcher<NewChannelPayload>(
+    'new-channel-payload'
 )
 
-export const isParticipantChangedEventPayload = generatePipelinePayloadMatcher(
-    'participant-changed-event-payload'
-)
+export const isNewMessageTimelineEventPayload = generatePipelinePayloadMatcher<
+    NewMessageTimelineEventPayload
+>('new-message-timeline-event-payload')
+export const isNewChannelTimelineEventPayload = generatePipelinePayloadMatcher<
+    NewChannelTimelineEventPayload
+>('new-channel-timeline-event-payload')
+
+export const isParticipantChangedEventPayload = generatePipelinePayloadMatcher<
+    ParticipationChangedEventPayload
+>('participant-changed-event-payload')
 
 export const isChannelStreamData = generatePipelinePayloadMatcher('stream-data')
 
