cab7323064f30d9cb63409b980376ca216002bac
Rohan Prabhu <rohan@mitter.io>
2018-10-08 22:44:43 +0530

    Fixing reconnection for websockets, use manual reconnection on a connection failure

--- packages/mitter-web/src/drivers/WebsocketMessagingPipelineDriver.ts
+++ packages/mitter-web/src/drivers/WebsocketMessagingPipelineDriver.ts
@@ -12,11 +12,17 @@ import { DeliveryEndpoint } from '@mitter-io/models'
 import SockJs from 'sockjs-client'
 import * as Stomp from '@stomp/stompjs'
 import { Message } from '@stomp/stompjs'
+import { noOp } from '../utils'
 
 export default class WebSocketPipelineDriver implements MessagingPipelineDriver {
     private activeSocket: Stomp.Client | undefined = undefined
     private pipelineSink: BasePipelineSink | undefined = undefined
     private connectionTime: number = 0
+    private mitterContext: Mitter | undefined = undefined
+
+    constructor() {
+        this.connectToStream = this.connectToStream.bind(this)
+    }
 
     endpointRegistered(pipelineSink: PipelineSink, userDeliveryEndpoint: DeliveryEndpoint): void {
         // Do nothing. For a driver not handling endpoints, this method will never be called
@@ -27,12 +33,13 @@ export default class WebSocketPipelineDriver implements MessagingPipelineDriver 
         return Promise.resolve(undefined)
     }
 
-    halt(): void {}
+    halt(): void {
+        noOp()
+    }
 
     initialize(mitter: Mitter): PipelineDriverInitialization {
-        const sockJs = new SockJs(`${mitter.mitterApiBaseUrl}/v1/socket/control/sockjs`)
-        this.activeSocket = Stomp.over(sockJs)
-        this.activeSocket.debug = () => {}
+        // this.activeSocket.debug = noOp
+        this.mitterContext = mitter
 
         return {
             pipelineDriverSpec: {
@@ -41,34 +48,7 @@ export default class WebSocketPipelineDriver implements MessagingPipelineDriver 
 
             initialized: new Promise((resolve, reject) => {
                 mitter.getUserAuthorization().then(userAuthorization => {
-                    if (userAuthorization === undefined || this.activeSocket === undefined) {
-                        reject(Error('Cannot construct websocket without user authorization'))
-                    } else {
-                        const authHeaders: any = {
-                            [StandardHeaders.UserAuthorizationHeader]: userAuthorization
-                        }
-
-                        if (mitter.applicationId !== undefined) {
-                            authHeaders[StandardHeaders.ApplicationIdHeader] = mitter.applicationId
-                        }
-
-                        this.activeSocket.reconnect_delay = 1000
-
-                        this.activeSocket.connect(
-                            authHeaders,
-                            frame => {
-                                this.activeSocket!!.subscribe(
-                                    '/user/event-stream',
-                                    this.processMessage.bind(this)
-                                )
-
-                                resolve(true)
-                            },
-                            error => {
-                                reject(error)
-                            }
-                        )
-                    }
+                    this.connectToStream(resolve, reject)
                 })
             })
         }
@@ -81,6 +61,76 @@ export default class WebSocketPipelineDriver implements MessagingPipelineDriver 
     private processMessage(wsMessage: Message) {
         if (this.pipelineSink !== undefined) {
             this.pipelineSink.received(JSON.parse(wsMessage.body))
+        }
+    }
+
+    private connectToStream(
+        resolveFn?: (result: boolean) => void,
+        rejectFn?: (err: Error | string) => void
+    ) {
+        if (this.mitterContext === undefined) {
+            if (rejectFn !== undefined) {
+                rejectFn(
+                    Error('Cannot connect to websocket, invalid mitter context (found undefined)')
+                )
+            } else {
+                console.warn(
+                    'Cannot connect to websocket, invalid mitter context.' +
+                        ' Also no error handlers were assigned to catch this error'
+                )
+            }
+        } else {
+            this.mitterContext.getUserAuthorization().then(userAuthorization => {
+                const sockJs = new SockJs(
+                    `${this.mitterContext!.mitterApiBaseUrl}/v1/socket/control/sockjs`
+                )
+                this.activeSocket = Stomp.over(sockJs)
+
+                let reject: (err: Error | string) => void = noOp
+                let resolve: (result: boolean) => void = noOp
+
+                if (resolveFn !== undefined) {
+                    resolve = resolveFn
+                }
+
+                if (rejectFn !== undefined) {
+                    reject = rejectFn
+                }
+
+                if (userAuthorization === undefined || this.activeSocket === undefined) {
+                    reject(Error('Cannot construct websocket without user authorization'))
+                } else {
+                    const authHeaders: any = {
+                        [StandardHeaders.UserAuthorizationHeader]: userAuthorization
+                    }
+
+                    if (this.mitterContext!.applicationId !== undefined) {
+                        authHeaders[
+                            StandardHeaders.ApplicationIdHeader
+                        ] = this.mitterContext!.applicationId
+                    }
+
+                    // this.activeSocket.reconnect_delay = 1000
+
+                    this.activeSocket.connect(
+                        authHeaders,
+                        frame => {
+                            this.activeSocket!!.subscribe(
+                                '/user/event-stream',
+                                this.processMessage.bind(this)
+                            )
+
+                            resolve(true)
+                        },
+                        error => {
+                            reject(error)
+                        },
+                        closeEvent => {
+                            setTimeout(this.connectToStream, 500)
+                        }
+                    )
+                }
+            })
         }
     }
 }
