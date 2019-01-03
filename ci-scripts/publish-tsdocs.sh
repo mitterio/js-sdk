1618ef6b4b4e8fe93ac6f00bd2d31ab607643209
Rohan Prabhu <rohan@mitter.io>
2019-01-03 18:10:39 +0530

    Adding s3 publishing for docs

---
+++ ci-scripts/publish-tsdocs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir ~/.aws
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = ${S3_DOCS_PUBLISH_ACCESS_KEY}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${S3_DOCS_PUBLISH_ACCESS_SECRET}" >> ~/.aws/credentials
+
+package_name=`npm list null | head -n1`
+package=`echo $package_name | cut -d'@' -f-2`
+version=`echo $package_name | cut -d'@' -f3 | cut -d' ' -f1`
+
+yarn run s3p sync -y -u docs/ "s3://mitter-sourcedocs/tsdocs/$package_name/$version"
+yarn run s3p sync -y -u docs/ "s3://mitter-sourcedocs/tsdocs/$package_name/latest"
+
