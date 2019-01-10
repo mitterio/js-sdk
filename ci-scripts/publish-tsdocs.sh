e451eecd8af18151eee6a500ce880847d7712b3c
Rohan Prabhu <rohan@mitter.io>
2019-01-10 15:03:08 +0530

    bump versisons

--- ci-scripts/publish-tsdocs.sh
+++ ci-scripts/publish-tsdocs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#mkdir ~/.aws
-#echo "[default]" >> ~/.aws/credentials
-#echo "aws_access_key_id = ${S3_DOCS_PUBLISH_ACCESS_KEY}" >> ~/.aws/credentials
-#echo "aws_secret_access_key = ${S3_DOCS_PUBLISH_ACCESS_SECRET}" >> ~/.aws/credentials
+mkdir ~/.aws
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = ${S3_DOCS_PUBLISH_ACCESS_KEY}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${S3_DOCS_PUBLISH_ACCESS_SECRET}" >> ~/.aws/credentials
 
 package_name=`npm list null | head -n1`
 package=`echo $package_name | cut -d'@' -f-2`
