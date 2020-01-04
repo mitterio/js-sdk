05f4d4ffae72d1c0802e95beabd1291740ae6a63
vineeth <vineeth@mitter.io>
2020-01-04 18:26:09 +0530

    publishing 0.7.4

--- packages/react-scl/src/!!ProducerInterface.ts
+++ packages/react-scl/src/!!ProducerInterface.ts
@@ -3,7 +3,6 @@ import { ReactElement } from 'react'
 export interface ProducerInterfaceConstructor<T> {
   new (condition: (item: T) => boolean, view: ReactElement<any>): ProducerInterface<T>
 }
-
 export interface ProducerInterface<T> {
   /*producerObj: {
     condition: (item: T) => boolean,
